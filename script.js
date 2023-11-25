62b0bd696371c78f53491358e525be2eeee29d6e
jellywilly420 <65194730+jellywilly420@users.noreply.github.com>
2023-11-26 00:11:17 +0200

    Create input fields Add ability to create new books from user input Add simple input verifaction

--- script.js
+++ script.js
@@ -1,22 +1,61 @@
 const library = [];
-function Book(title, author, readingState){
-    this.title = title;
-    this.author = author;
-    this.readingState = readingState;
+const dialog = document.querySelector("dialog");
+const openButton = document.querySelector("button#open");
+const closeButton = document.querySelector("button#close");
+const addButton = document.querySelector("button#add");
+const titleInput = document.querySelector("input#title");
+const authorInput = document.querySelector("input#author");
+const progressButtons = [...document.querySelectorAll('input[type="radio"]')];
 
-    this.bookIndex = function() {
-        // if (library.indexOf(this) === -1) {
-        //     return null;
-        // }
-        return library.indexOf(this);
-    }
-    this.addToLibrary = function() {
-        library.push(this);
-    }
-    this.removeFromLibrary = function() {
-        library.splice(cookingBook.bookIndex(), 1);
-    }
-};
+function Book(title, author, readingState) {
+  this.title = title;
+  this.author = author;
+  this.readingState = readingState;
+
+  this.bookIndex = function () {
+    return library.indexOf(this);
+  };
+  this.addToLibrary = function () {
+    library.push(this);
+  };
+  this.removeFromLibrary = function () {
+    library.splice(cookingBook.bookIndex(), 1);
+  };
+}
+
+function clearValue(input) {
+  input.value = "";
+}
+function createBook() {
+    const title = titleInput.value;
+    const author = authorInput.value;
+    const progress = progressButtons.filter((button) => {
+      if (button.checked) {
+        return button;
+      }
+    })[0].value;
+    return new Book(title, author, progress)
+}
+
+openButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+closeButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  clearValue(titleInput);
+  clearValue(authorInput);
+  dialog.close();
+});
+
+addButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (titleInput.value === "" || authorInput.value === "") {
+    alert("Please fill all the fields.");
+  } else {
+    const newBook = createBook();
+    newBook.addToLibrary();
+}
+});
 
 // const gamerBook = new Book('gaming book', 'gamer author', 'reading');
 // const cookingBook = new Book('cooking book', 'chef author', 'not started');
@@ -25,4 +64,3 @@ function Book(title, author, readingState){
 // cookingBook.addToLibrary();
 // cookingBook.removeFromLibrary();
 // console.log(cookingBook.bookIndex());
-
