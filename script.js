fccc0228572483fb1aaca818796644b00096fffb
jellywilly420 <65194730+jellywilly420@users.noreply.github.com>
2023-11-28 07:38:39 +0200

    Add simple responsive mobile layout
    Change card colors

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const library = [];
-const main = document.querySelector('main');
+const main = document.querySelector("main");
 const dialog = document.querySelector("dialog");
 const openButton = document.querySelector("header button#open");
 const closeButton = document.querySelector("button#close");
@@ -7,13 +7,17 @@ const addButton = document.querySelector("button#add");
 const titleInput = document.querySelector("input#title");
 const authorInput = document.querySelector("input#author");
 const progressButtons = [...document.querySelectorAll('input[type="radio"]')];
-const requiredTitle = document.querySelector('.title-container .required-title');
-const requiredAuthor = document.querySelector('.author-container .required-author');
+const requiredTitle = document.querySelector(
+  ".title-container .required-title"
+);
+const requiredAuthor = document.querySelector(
+  ".author-container .required-author"
+);
 
-const div = document.createElement('div');
-const h2 = document.createElement('h2');
-const p = document.createElement('p');
-const button = document.createElement('button');
+const div = document.createElement("div");
+const h2 = document.createElement("h2");
+const p = document.createElement("p");
+const button = document.createElement("button");
 
 function Book(title, author, readingProgress) {
   this.title = title;
@@ -33,17 +37,17 @@ function Book(title, author, readingProgress) {
     const removeButton = button.cloneNode();
     const changeProgressButton = button.cloneNode();
     titleNode.innerText = this.title;
-    autherNodeP.innerText = 'by ' + this.author;
+    autherNodeP.innerText = "by " + this.author;
     progressNode.innerText = this.readingProgress;
-    cardNode.classList.add('card');
-    progressNode.classList.add('progress-node');
-    progressNode.setAttribute('data-progress', this.readingProgress)
-    buttonContainer.classList.add('button-container');
-    removeButton.classList.add('delete-button');
+    cardNode.classList.add("card");
+    progressNode.classList.add("progress-node");
+    progressNode.setAttribute("data-progress", this.readingProgress);
+    buttonContainer.classList.add("button-container");
+    removeButton.classList.add("delete-button");
     removeButton.innerText = "remove book";
-    changeProgressButton.classList.add('change-progress-button');
+    changeProgressButton.classList.add("change-progress-button");
     changeProgressButton.innerText = "change progress";
-    cardNode.setAttribute('data-title', this.title);
+    cardNode.setAttribute("data-title", this.title);
     cardNode.appendChild(progressNode);
     cardNode.appendChild(titleNode);
     cardNode.appendChild(autherNodeP);
@@ -52,33 +56,33 @@ function Book(title, author, readingProgress) {
     buttonContainer.appendChild(removeButton);
     main.appendChild(cardNode);
 
-    removeButton.addEventListener('click',()=>{
-        this.removeFromLibrary()
-    })
-    changeProgressButton.addEventListener('click', ()=>{
-        if(this.readingProgress === 'to read') {
-            this.readingProgress = 'reading';
+    removeButton.addEventListener("click", () => {
+      this.removeFromLibrary();
+    });
+    changeProgressButton.addEventListener("click", () => {
+      if (this.readingProgress === "to read") {
+        this.readingProgress = "reading";
+      } else if (this.readingProgress === "reading") {
+        this.readingProgress = "finished reading";
+      } else {
+        this.readingProgress = "to read";
+      }
+      progressNode.setAttribute("data-progress", this.readingProgress);
+      for (const progressNode of main.querySelectorAll("p.progress-node")) {
+        if (
+          progressNode.parentElement.getAttribute("data-title") === this.title
+        ) {
+          progressNode.innerText = this.readingProgress;
         }
-        else if(this.readingProgress === 'reading') {
-            this.readingProgress = 'finished reading';
-        }
-        else {
-            this.readingProgress = 'to read';
-        }
-        progressNode.setAttribute('data-progress', this.readingProgress)
-        for (const progressNode of main.querySelectorAll('p.progress-node')) {
-            if (progressNode.parentElement.getAttribute('data-title') === this.title) {
-                progressNode.innerText = this.readingProgress;
-            }
-        }
-    })
+      }
+    });
   };
   this.removeFromLibrary = function () {
     library.splice(this.bookIndex(), 1);
     for (const card of main.childNodes) {
-        if (card.getAttribute('data-title') === this.title) {
-            main.removeChild(card);
-        }
+      if (card.getAttribute("data-title") === this.title) {
+        main.removeChild(card);
+      }
     }
   };
 }
@@ -105,50 +109,51 @@ openButton.addEventListener("click", () => {
 closeButton.addEventListener("click", (event) => {
   event.preventDefault();
   clearValues();
-  requiredAuthor.classList.add('hidden');
-  requiredTitle.classList.add('hidden');
+  requiredAuthor.classList.add("hidden");
+  requiredTitle.classList.add("hidden");
   dialog.close();
 });
 addButton.addEventListener("click", (event) => {
   event.preventDefault();
   if (authorInput.value === "" && titleInput.value === "") {
-    requiredTitle.classList.remove('hidden');
-    requiredAuthor.classList.remove('hidden');
+    requiredTitle.classList.remove("hidden");
+    requiredAuthor.classList.remove("hidden");
     clearValues();
-  } 
-  else if (titleInput.value === "") {
-    requiredTitle.classList.remove('hidden');
+  } else if (titleInput.value === "") {
+    requiredTitle.classList.remove("hidden");
     clearValues();
-  }
-  else if (authorInput.value === "") {
-    requiredAuthor.classList.remove('hidden');
+  } else if (authorInput.value === "") {
+    requiredAuthor.classList.remove("hidden");
     clearValues();
-  }
-  else {
+  } else {
     let bookInLibrary = false;
     for (const book of library) {
-      if (book.title === titleInput.value && book.author === authorInput.value) {
+      if (
+        book.title === titleInput.value &&
+        book.author === authorInput.value
+      ) {
         bookInLibrary = true;
       }
     }
     if (bookInLibrary) {
-      alert (`${titleInput.value} by ${authorInput.value} is already in the library.`);
+      alert(
+        `${titleInput.value} by ${authorInput.value} is already in the library.`
+      );
       clearValues();
-    }
-    else {
+    } else {
       createBook().addToLibrary();
       clearValues();
       dialog.close();
     }
   }
 });
-titleInput.addEventListener('focus', ()=>{
-  if (!requiredTitle.classList.contains('hidden')) {
-    requiredTitle.classList.add('hidden');
+titleInput.addEventListener("focus", () => {
+  if (!requiredTitle.classList.contains("hidden")) {
+    requiredTitle.classList.add("hidden");
   }
-})
-authorInput.addEventListener('focus', ()=>{
-  if (!requiredAuthor.classList.contains('hidden')) {
-    requiredAuthor.classList.add('hidden');
+});
+authorInput.addEventListener("focus", () => {
+  if (!requiredAuthor.classList.contains("hidden")) {
+    requiredAuthor.classList.add("hidden");
   }
-})
+});
